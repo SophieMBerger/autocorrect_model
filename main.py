f676106a02f8b9ff5c989dedcf09038ea0c0fb24
Sophie Berger <berger.m.sophie@gmail.com>
2024-07-29 13:57:27 -0700

    MiniLM fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import time
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from transformers import AutoModelForMaskedLM, AutoTokenizer, TFAutoModel
+from transformers import TFAutoModelForMaskedLM, AutoTokenizer, TFAutoModel
 import tensorflow as tf
 import numpy as np
 import Levenshtein
